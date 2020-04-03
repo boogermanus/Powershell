2159e684ace031839421c7f861899830d5d71218
jordan.woodruff <Jordan.Woodruff@tylertech.com>
2020-04-02 19:31:59 -0500

    open code on -noserve and remove existing

--- git-install-serve.ps1
+++ git-install-serve.ps1
@@ -8,9 +8,16 @@ param(
 $temp = "\code\angular\temp";
 
 Push-Location $temp
+
 #match https://github.com/user/(repo) or https://github.com/user/(some-repo) or https://github.com/user/(repo2)
 #or https://github.com/user/(repo2_repo)
 $newDir = [regex]::Match($repo, "([0-9|A-Z|a-z|-|_]*)\/*$");
+
+if(Test-Path $newDir)
+{
+    "Removing existing clone"
+    Remove-Item $newDir -Force -Recurse
+}
 
 "Cloning"
 git clone $repo
@@ -24,6 +31,10 @@ if (-not $noserve)
 {
     "Serving"
     ng serve
+}
+else
+{
+    code .
 }
 
 
