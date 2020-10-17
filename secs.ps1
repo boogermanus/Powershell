2be9a1ca1469efecb1433778b7f7a0c3126f1dbb
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-10-16 20:43:41 -0500

    secs

---
+++ secs.ps1
@@ -0,0 +1,16 @@
+#usage: secs -date 10/29/81 -span G
+param(
+		[Parameter()]
+		[DateTime]$date,
+		[Parameter()]
+		[ValidateSet("K","M","G")]
+		[string]$span
+)
+
+$spans = @{"K" = [Math]::Pow(10,3); "M" = [Math]::Pow(10,6); "G" = [Math]::Pow(10,9)}
+
+$timeSpan = new-timespan $date (Get-Date)
+
+$answer = [Math]::Round([Math]::Abs($timeSpan.TotalSeconds)/$spans[$span],3)
+
+"$answer $span" + "s"
