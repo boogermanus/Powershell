2f38df41e02853847b03ad6ea4e451455e48fa7b
jordan.woodruff <jordan.woodruff@tylertech.com>
2023-02-17 20:18:18 -0600

    Updating progress bar example

--- progress-bar.ps1
+++ progress-bar.ps1
@@ -1,5 +1,28 @@
-for($i = 0;$i -lt 400;$i++)
-{
-    $complete = ($i/400) * 100
-    Write-Progress -Activity "test" -status "$complete%" -PercentComplete (($i/400) * 100)
+$oneHundred = 100;
+
+#shows percent complete
+for ($i = 0; $i -lt $oneHundred; $i++) {
+    Start-Sleep -Milliseconds 10
+    $complete = ($i / $oneHundred) * 100
+    Write-Progress -Activity "Doing stuff.." -PercentComplete $complete
+}
+
+#shows percent complete in a for each
+$stuff = @(Get-Process)
+$counter = 0;
+
+foreach($thing in $stuff) {
+    Start-Sleep -Milliseconds 10
+    $counter++
+    $complete = ($counter/$stuff.Length) * 100;
+    Write-Progress -Activity "Doing stuff..." -PercentComplete $complete
+}
+
+#show percent with a message
+$counter = 0
+foreach($thing in $stuff) {
+    Start-Sleep -Milliseconds 10
+    $counter++
+    $complete = ($count/$stuff.Length) * 100;
+    Write-Progress -Activity "Doing stuff..." -Status "Doing stuff to $($thing.Name)" -PercentComplete $complete
 }
