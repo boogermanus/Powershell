95e2f1dc6ead658d946056d423b0a69b05b25f2f
jordan.woodruff <jordan.woodruff@tylertech.com>
2023-02-17 19:28:24 -0600

    Clean Dotnet

---
+++ clean-dotnet.ps1
@@ -0,0 +1,16 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$path
+)
+
+$items = Get-ChildItem -path $path -Include "bin","obj" -Exclude "node_modules" -Recurse -Directory
+$count = $items.Length;
+$i = 0;
+
+foreach($item in $items)
+{
+    $status = ($i/$count) * 100
+    Write-Progress -Activity "Removing files in $($item.FullName)" -PercentComplete $status
+    $item | Remove-Item -WhatIf -Recurse -Force -Verbose
+    $i++;
+}
