31e9de397d502ea4c485e5bf93dcaf87c72a9f89
Jordan Woodruff <boogermanus@gmail.com>
2025-06-23 15:23:16 -0500

    changes

--- camelia-scan.ps1
+++ camelia-scan.ps1
@@ -5,18 +5,32 @@ param(
     [string]$images = 'D:\images'
 )
 
-$files = Get-ChildItem -r $path -file
+$files = Get-ChildItem -r $path
 
-foreach($file in $files) {
-    if($file.PSIsContainer) {
-        "directory $($file.FullName)"
-    }
-    else {
-        if($file.Extension -eq ".zip") {
-            "zip $($file.FullName)"
+function scan([System.IO.FileInfo[]]$items) {
+    foreach($item in $items) {
+        if($item.PSIsContainer) {
+            scan-directory 
         }
         else {
-            "file $($file.FullName)"
+            if($file.Extension -eq ".zip") {
+                scan-zip $file
+            }
+            else {
+                "file $($file.FullName)"
+            }
         }
     }
+}
+
+function scan-directory([System.IO.FileInfo]$dir) {
+    "directory $($dir.FullName)"
+}
+
+function scan-zip([System.IO.FileInfo]$zip) {
+    "zip $($zip.FullName)"
+}
+
+function handle-file([System.IO.FileInfo]$file) {
+    "file $($file.FullName)"
 }
